7c0ac56490a76cfad84a358700dd3cb20f80f98d
samuel nicolau <samuel.labjt@gmail.com>
2025-06-09 01:30:37 -0300

    teste12

--- src/main/java/configuracoes/ConfigJDBC.java
+++ src/main/java/configuracoes/ConfigJDBC.java
@@ -17,7 +17,7 @@ public class ConfigJDBC {
 		}
         String url = "jdbc:postgresql://dpg-d133o8juibrs73fpmb00-a.ohio-postgres.render.com:5432/arcana_bx1u";
         String user = "arcana_user";
-        String password = "VaggJuE9Xr3qlFNGT312Js2LzMy6rFIE.";
+        String password = "VaggJuE9Xr3qlFNGT312Js2LzMy6rFIE";
         Connection conn = DriverManager.getConnection(url, user, password);
         System.out.println("Conexão estabelecida com sucesso!");
         return conn;
